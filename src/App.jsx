5c1e0fb3de15daea8127663f0222d31b5e3b7ef9
Guilherme <guilhermesobreiro9@gmail.com>
2023-09-05 11:48:46 -0300

    1.1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Exemplo2 from './Exemplo2'
 import Exemplo3 from './Exemplo3'
 import Nav from './Nav'
 import Footer from './Footer'
+import Notfound from './Notfound'
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <Route path="/Exemplo1" element={<Exemplo1 />}/>
       <Route path="/Exemplo2" element={<Exemplo2 />}/>
       <Route path="/Exemplo3" element={<Exemplo3 />}/>
+      <Route path='*' element={<Notfound />}/>
     </Routes>
     <Footer />
     </BrowserRouter>
---
+++ src/Notfound.jsx
@@ -0,0 +1,10 @@
+import {} from 'react'
+
+function Notfound(){
+    return(
+        <>
+        <h1>PÁGINA NÃO ENCONTRADA !!!</h1>
+        </>
+    )
+}
+export default Notfound
